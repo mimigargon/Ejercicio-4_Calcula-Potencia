84783337afa2044c31aa76d838048494b2dbf3fc
Mimi <mireiag77@gmail>
2023-06-07 16:18:09 +0200

    Problem solved

--- Ejercicio-4_Calcula-Potencia/Program.cs
+++ Ejercicio-4_Calcula-Potencia/Program.cs
@@ -10,38 +10,41 @@ namespace Ejercicio_4_Calcula_Potencia
     {
         static void Main(string[] args)
         {
-            double[] b = new double[10];
-            double[] e = new double[10];
+            double result = 1;
+            double negativeResult;
 
-            Random rand = new Random();
-             for(int i = 0; i < b.Length; i++)
+            Console.WriteLine("Introduce la base: ");
+            int baseNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduce el exponente: ");
+            int exponent = int.Parse(Console.ReadLine());
+
+            if(exponent < 0)
             {
-                b[i] = rand.Next(-0, 1001);
+                exponent = exponent * -1;
 
-             for(int j = 0; j < e.Length; j++)
-            {
-                e[j] = rand.Next(-0, 1001);
-                
-                Console.WriteLine("La potencia de {0} elevado a {1} es: {2} ", b[i], e[j], calcularPotencia(b, e));
-            }
-
-            }
-
-
-        }
-
-        public static double calcularPotencia(double[] arr1, double[] arr2)
-        {
-            double result = 0;
-            for(int i = 0; i < arr1.Length; i++)
-            {
-                for(int j = 0; j < arr2.Length; j++)
+                for(int i = 1; i <= exponent; i++)
                 {
-                    result = Math.Pow(arr1[i], arr2[j]);
+                    result *= baseNum;
                 }
-            }
 
-            return result;
+                negativeResult = (1 / result);
+
+                Console.WriteLine("{0}^{1} = {2}", baseNum, exponent, negativeResult);
+
+            } else
+            {
+                for (int i = 1; i <= exponent; i++)
+                {
+                    result = result * baseNum;
+                }
+
+                Console.WriteLine("{0}^{1} = {2}", baseNum, exponent, result);
+            }
+            
+
         }
-    }
+
+
+    }   
 }
